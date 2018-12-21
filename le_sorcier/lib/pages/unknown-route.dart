12ef635fbfbe13aa35e73a6310a12449193526a5
Alde <iObsidian@users.noreply.github.com>
2018-12-21 17:11:02 -0500

    Added placeholder fallback page for unknown route and auth page

---
+++ le_sorcier/lib/pages/unknown-route.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class UnknownRoute extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Unknown page.")); //TODO Implement 404 page
+  }
+}
